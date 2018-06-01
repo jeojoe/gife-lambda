0b8f6d7e6ecefb33d7bcd62f76eda197d0538ada
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-02 00:31:19 +0700

    Default value in http res lib

--- src/libs/httpResponse.js
+++ src/libs/httpResponse.js
@@ -9,10 +9,10 @@ function buildResponse(statusCode, body) {
   };
 }
 
-export function success(body) {
+export function success(body = null) {
   return buildResponse(200, body);
 }
 
-export function failure(statusCode = 500, body) {
+export function failure(statusCode = 500, body = null) {
   return buildResponse(statusCode, body);
 }
