47895c9ed472a57b52a5d8caa8d50197e5e6e847
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-30 15:10:53 +0700

    Fix data on spotlight challenge

--- src/challenge.js
+++ src/challenge.js
@@ -95,8 +95,28 @@ export const getExplore = async (event, context, callback) => {
     // Query spotlight & types
     const [spotlightResult, typesResult] = await dbClient.query(`
       -- Get spotlight --
-      SELECT *
+      SELECT
+        challenges.id as id,
+        challenges.created_at as created_at,
+        challenges.updated_at as updated_at,
+        start_date,
+        end_date,
+        goal_description,
+        banner_image_url,
+        location_label,
+        rating,
+        reward_gife_points,
+        reward_id,
+        is_spotlight,
+        is_sponsored,
+        minimum_completed,
+        challenges.title as title,
+        challenge_types.title as type_title,
+        challenge_durations.title as duration_title,
+        duration_in_hours
       FROM challenges
+        INNER JOIN challenge_types
+          ON challenges.challenge_type_id = challenge_types.id
         INNER JOIN challenge_durations
           ON challenge_durations.id = challenges.challenge_duration_id
       WHERE is_spotlight = true
